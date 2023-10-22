2c119c7a3793aef72947a39d9c451638ba2478fb
JW <daxon2012@gmail.com>
2023-10-21 19:19:37 -0700

    Adding connection function

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,12 @@
-// I am getting the Mysql npm package 
+// I am importing mysql2 from the npm package and assigned it to the constant of mysql
+// This package provides functionalities to work with MySQL databases in Node.js
 const mysql = require("mysql2");
 
+// I am assigning mysql.createConnection() to the constant connect
+// The mysql.createConnection() function is a method in 'mysql2' package 
+    // it create a new connection to MySQL Database(db)
+// host: is where my MySQL server is running. I'm setting it to localhost
+// localhost: setting the host to localhost means the server is running on the same machine as Node.js application
+const connection = mysql.createConnection({
+
+})
