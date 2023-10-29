5b2954777074212186d46f1705497c7dfd1c668c
JW <daxon2012@gmail.com>
2023-10-29 03:05:54 -0700

    Creating schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- The `DROP DATABASE IF EXISTS` is MySQL language
+    -- It will delete the db which I name "employees_db" if it already exist
+    -- This service as formatting the document so I could start fresh
+DROP DATABASE IF EXISTS employees_db; 
